46012fc88517558ff90c6bc5ee20279d512c67be
Roy Lu <royvbtw@gmail.com>
2017-01-21 22:09:50 +0800

    add connection ability

--- echo.js
+++ echo.js
@@ -7,6 +7,13 @@ function echo(msg){
 	console.log("message is " + msg);
 	
 	console.log("server offline.");		// server branch code
+	if(status === 0){
+		console.log("Local backup is running.");
+	}else if(status === 1){
+		console.log("Connect with remote database.");
+	}else{
+		console.log("ERROR");
+	}
 	return null;
 }
 
