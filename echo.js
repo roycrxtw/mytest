92edda1f473a80c8bed3af5e1c2e4f3dc900313d
Roy Lu <royvbtw@gmail.com>
2017-01-21 22:04:04 +0800

    add status

--- echo.js
+++ echo.js
@@ -1,6 +1,7 @@
 // rebase test
 
 function echo(msg){
+	var status = 0;
 	console.log("Basic echo function.");
 	console.log("Server is running.");		// server branch code.
 	console.log("message is " + msg);
