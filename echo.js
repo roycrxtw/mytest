15e0c4343ce5c5117bf283645a1eea1fbe60d3d4
Roy Lu <royvbtw@gmail.com>
2017-01-21 21:56:00 +0800

    add fix

--- echo.js
+++ echo.js
@@ -4,4 +4,6 @@ function echo(msg){
 	console.log("Basic echo function.");
 	console.log("Server is running.");		// server branch code.
 	console.log("message is " + msg);
+	
+	console.log("do some fix.");		// fix branch code.
 }
