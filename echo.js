1bf4b25d0c08d76da1e0f1298e09c1466d383747
Roy Lu <royvbtw@gmail.com>
2017-01-21 21:50:57 +0800

    add offline message

--- echo.js
+++ echo.js
@@ -4,4 +4,7 @@ function echo(msg){
 	console.log("Basic echo function.");
 	console.log("Server is running.");		// server branch code.
 	console.log("message is " + msg);
+	
+	console.log("server offline.");
 }
+
